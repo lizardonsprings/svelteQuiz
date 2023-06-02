5aded1bb802a917f959b7792139f551a9ac9f554
Brendan O'Reilly <bren@aliensoap.co.uk>
2023-06-02 17:04:46 +0100

    second commit

--- src/App.svelte
+++ src/App.svelte
@@ -2,5 +2,12 @@
 	import Quiz from "./Quiz.svelte";
 </script>
 
+<style>
+	:global(h4) {
+		color: green;
+	}
+</style>
+
 <h1>How you been!</h1>
+<h4>h4</h4>
 <Quiz />
--- src/Quiz.svelte
+++ src/Quiz.svelte
@@ -1,8 +1,13 @@
 <script>
 
+        // stuck on video number 9, would appear the quiz API is
+        // not feeding through as expected, can't seem to get
+        // the results etc to show in my debugger - next thing to sort
+
     let result;
     let correctAnswer = "b";
-    let answers = ["a", "b", "c", "d"]
+    let answers = ["a", "b", "c", "d"];
+    let quiz = getQuiz();
 
     function pickAnswer(answer) {
         if (answer === correctAnswer) {
@@ -11,17 +16,37 @@
         result = "NA M8"
     }
 
+    async function getQuiz() {
+        const res = await fetch('https://opentdb.com/api.php?amount=10&category=12&difficulty=medium&type=multiple');
+        const quiz = await res.json();
+        debugger;
+    }
+
 </script>
 
+<style>
+    /* h4 {
+        color: red;
+    } */
+
+</style>
+
 <div>
+
+    <button on:click={getQuiz}>Get questions</button>
 
     {#if result}
     <h4>{result}</h4>
     {:else}
-        <h5>pick an answer</h5>
+        <h4>pick an answer</h4>
     {/if}
+
+    <h3>{quiz.results[0].question}</h3>
+
     {#each answers as answer}
-     <button on:click={() => pickAnswer(answer)}>Answer {answer.toUpperCase()}</button>
-    {/each}
+     <button on:click={() => pickAnswer(answer)}>
+        Answer {answer.toUpperCase()}
+    </button>
+    {/each} 
 
 </div>
