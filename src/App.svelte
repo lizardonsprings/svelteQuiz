4328e42c4b481ecc28c9b6881e26329f71d69da8
Brendan O'Reilly <bren@aliensoap.co.uk>
2023-06-02 16:24:45 +0100

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,6 @@
+<script>
+	import Quiz from "./Quiz.svelte";
+</script>
+
+<h1>How you been!</h1>
+<Quiz />
---
+++ src/Quiz.svelte
@@ -0,0 +1,27 @@
+<script>
+
+    let result;
+    let correctAnswer = "b";
+    let answers = ["a", "b", "c", "d"]
+
+    function pickAnswer(answer) {
+        if (answer === correctAnswer) {
+           return result = "Correct!"
+        }
+        result = "NA M8"
+    }
+
+</script>
+
+<div>
+
+    {#if result}
+    <h4>{result}</h4>
+    {:else}
+        <h5>pick an answer</h5>
+    {/if}
+    {#each answers as answer}
+     <button on:click={() => pickAnswer(answer)}>Answer {answer.toUpperCase()}</button>
+    {/each}
+
+</div>
